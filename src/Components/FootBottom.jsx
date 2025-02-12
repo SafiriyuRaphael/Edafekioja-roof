5c4a185a6e4d4c43342eeb033b1c25314b9c0b44
safiriyuraphael <toniaroyce@gmail.com>
2025-02-12 10:08:25 +0100

    commit

--- src/Components/FootBottom.jsx
+++ src/Components/FootBottom.jsx
@@ -8,12 +8,12 @@ const FootBottom = () => {
         Copyright and Legal Information &copy; 2025 Edafekioja. <span className="whitespace-nowrap">All rights reserved.</span> 
       </p>
       <ul className="flex gap-2 sm:gap-3 m-0 mt-3 sm:mt-0 justify-center sm:justify-start">
-        <li><Link to="/" className="text-black">Home</Link></li>
-        <li><Link to="/about" className="text-black">About</Link></li>
-        <li><Link to="/services" className="text-black">Services</Link></li>
-        <li><Link to="/projects" className="text-black">Projects</Link></li>
-        <li><Link to="/contacts" className="text-black white whitespace-nowrap">Contact Us</Link></li>
-        <li><Link to="/faqs" className="text-black">FAQs</Link></li>
+        <li><Link to="/" className="text-black hover:text-white">Home</Link></li>
+        <li><Link to="/about" className="text-black hover:text-white">About</Link></li>
+        <li><Link to="/services" className="text-black hover:text-white">Services</Link></li>
+        <li><Link to="/projects" className="text-black hover:text-white">Projects</Link></li>
+        <li><Link to="/contacts" className="text-black hover:text-white white whitespace-nowrap">Contact Us</Link></li>
+        <li><Link to="/faqs" className="text-black hover:text-white">FAQs</Link></li>
       </ul>
     </section>
   );
